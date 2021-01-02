f4f7a544b591b792a7ecee9630283ba03a1f7467
hp <hp@HP01>
2021-01-02 16:02:43 +0000

    added a comment

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -5,6 +5,8 @@ import java.util.Random;
 
 public class Hangman {
 
+    //comment spd
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
